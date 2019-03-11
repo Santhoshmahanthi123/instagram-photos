bdc03c33d78d06f8dded1d2e00e24990bd337fa1
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-11 19:48:11 +0530

    access token error

--- app.js
+++ app.js
@@ -5,11 +5,58 @@ require("dotenv").config();
 const express = require("express"),
   bodyParser = require("body-parser"),
   app = express().use(bodyParser.json()); // creates express http server
-
+const ig = require("instagram-node").instagram();
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+//the redirect uri we set when registering our application
+const redirectUri = "http://localhost:3000/handleAuth";
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("welcome to instagram photos");
+app.use(express.static(__dirname + "/public"));
+//set the view engine to use ejs
+app.set("view engine", "ejs");
+
+ig.use({
+  client_id: clientId,
+  client_secret: clientSecret
 });
+app.get("/authorize", function(req, res) {
+  // set the scope of our application to be able to access likes and public content
+  res.redirect(
+    ig.get_authorization_url(redirectUri, {
+      scope: ["public_content", "likes"]
+    })
+  );
+});
+app.get("/handleAuth", function(req, res) {
+  //retrieves the code that was passed along as a query to the '/handleAuth' route and uses this code to construct an access token
+  ig.authorize_user(req.query.code, redirectUri, function(err, result) {
+    if (err) res.send(err);
+    // store this access_token in a global variable called accessToken
+    var accessToken = result.access_token;
+    // After getting the access_token redirect to the '/' route
+    res.redirect("/");
+  });
+});
+
+app.get("/", function(req, res) {
+  // create a new instance of the use method which contains the access token gotten
+  ig.use({
+    access_token: accessToken
+  });
+
+  ig.user_media_recent(access_token.split(".")[0], function(
+    err,
+    result,
+    pagination,
+    remaining,
+    limit
+  ) {
+    if (err) res.json(err);
+    // pass the json file gotten to our ejs template
+    res.render("pages/index", { instagram: result });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
